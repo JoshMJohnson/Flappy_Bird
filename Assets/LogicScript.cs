c9cd520b36a2e028b1a042ecfa5f52e6ec76d94b
JoshMJohnson <joshjohns21@gmail.com>
2024-03-12 23:43:03 -0500

    keeps score

---
+++ Assets/LogicScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore = 0;
+    public Text scoreText;
+
+    private void Start()
+    {
+        playerScore = 0;
+        scoreText.text = "0";
+    }
+
+    [ContextMenu("Increase Score")]
+    public void addScore()
+    {
+        playerScore++;
+        scoreText.text = playerScore.ToString();
+    }
+}
